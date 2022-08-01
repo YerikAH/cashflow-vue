f2439c71ca9aa3419df412c7e2c3a903d2e38e41
Harvey Yerik <yerik05vh@gmail.com>
2022-08-01 16:08:12 -0500

    splashscreen

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template lang="">
+  <div>
+    <Suspense>
+      <template #default><Home /></template>
+      <template #fallback> <SplashScreenVue /></template>
+    </Suspense>
+  </div>
+</template>
+<script>
+import Home from "./components/Home.vue";
+import SplashScreenVue from "./components/SplashScreen.vue";
+
+import { defineAsyncComponent } from "vue";
+export default {
+  components: {
+    SplashScreenVue,
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
+  },
+};
+</script>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="splashscreen">
+    <img src="../assets/logo.svg" alt="logo" />
+    <h1><span>Cash</span>Flow</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
